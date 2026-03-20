b9324b898dc998046a5a5b99fa417b93a16f8d84
Carl Valdemar Ebbesen <cve@progit.no>
2026-03-20 07:50:30 +0100

    use correct env

--- src/routers/public_router.ts
+++ src/routers/public_router.ts
@@ -1,40 +1,43 @@
-import { Hono } from 'hono';
-import { TPublicEnv, TStravaEnv } from '../types/IRouters';
-import { env } from 'bun';
-import { processStravaWebhook } from '../services.ts/process_strava_event';
-import { IStravaWebhookEvent } from '../types/strava/IWebHookEvent';
-import { stravaMiddleware } from '../middlewares/strava_middleware';
+import { env } from "bun";
+import { Hono } from "hono";
+import { processStravaWebhook } from "../services.ts/process_strava_event";
+import type { TPublicEnv } from "../types/IRouters";
+import type { IStravaWebhookEvent } from "../types/strava/IWebHookEvent";
 
 const publicRouter = new Hono<TPublicEnv>();
 
 publicRouter.get("/strava/event", (c) => {
-  console.log("Strava handshake triggered");
-  const mode = c.req.query("hub.mode");
-  const token = c.req.query("hub.verify_token");
-  const challenge = c.req.query("hub.challenge");
-  const expectedToken = process.env.STRAVA_WEBHOOK_VERIFY_TOKEN;
-  console.log(`Handshake - mode: ${mode}, token match: ${token === expectedToken}, challenge: ${challenge}`);
-  if (mode === "subscribe" && token === expectedToken) {
-    return c.json({ "hub.challenge": challenge }, 200);
-  }
-  console.error("Verification failed: Token mismatch or invalid mode");
-  return c.json({ error: "Verification failed" }, 403);
+	console.log("Strava handshake triggered");
+	const mode = c.req.query("hub.mode");
+	const token = c.req.query("hub.verify_token");
+	const challenge = c.req.query("hub.challenge");
+	const expectedToken = env.STRAVA_WEBHOOK_VERIFY_TOKEN;
+	console.log(
+		`Handshake - mode: ${mode}, token match: ${token === expectedToken}, challenge: ${challenge}`,
+	);
+	if (mode === "subscribe" && token === expectedToken) {
+		return c.json({ "hub.challenge": challenge }, 200);
+	}
+	console.error("Verification failed: Token mismatch or invalid mode");
+	return c.json({ error: "Verification failed" }, 403);
 });
 publicRouter.get("/health", (c) => {
-  console.log("Health triggered");
-  return c.json("i'm alive :D ", 200);
+	console.log("Health triggered");
+	return c.json("i'm alive :D ", 200);
 });
 publicRouter.post("/strava/event", async (c) => {
-  const body = (await c.req.json()) as IStravaWebhookEvent;
+	const body = (await c.req.json()) as IStravaWebhookEvent;
 
-  console.log(`Strava event receivied: Type: ${body.aspect_type}, eventId: ${body.object_id} for athlete: ${body.owner_id}`);
-  if (body.subscription_id?.toString() !== env.STRAVA_SUBSCRIPTION_ID) {
-    return c.json({ status: "unauthorized" }, 401);
-  }
-  processStravaWebhook(body, c.env).catch(err => 
-    console.error("Background processing failed:", err)
-  );
-  return c.json({ status: "ok" }, 200);
+	console.log(
+		`Strava event receivied: Type: ${body.aspect_type}, eventId: ${body.object_id} for athlete: ${body.owner_id}`,
+	);
+	if (body.subscription_id?.toString() !== env.STRAVA_SUBSCRIPTION_ID) {
+		return c.json({ status: "unauthorized" }, 401);
+	}
+	processStravaWebhook(body, c.env).catch((err) =>
+		console.error("Background processing failed:", err),
+	);
+	return c.json({ status: "ok" }, 200);
 });
 
 publicRouter.route("/", publicRouter);
